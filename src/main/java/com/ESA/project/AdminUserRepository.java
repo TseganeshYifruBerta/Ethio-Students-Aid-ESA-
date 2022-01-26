c6a1a7f64d9510dacac19f444a246cc284c06471
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-01-26 22:56:42 +0300

    Create AdminUserRepository.java

---
+++ src/main/java/com/ESA/project/AdminUserRepository.java
@@ -0,0 +1,9 @@
+// package com.ESA.project.Admin;
+
+// // import com.ESA.project.security.User;
+
+// import org.springframework.data.repository.CrudRepository;
+
+// public interface AdminUserRepository extends CrudRepository<AdminUsers, Long> {
+//     public AdminUsers findByUsername(String username);
+// }
