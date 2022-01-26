3b61af4f34373aa1b446c640acb81bee3be2196d
Tseganesh Yifru <88632885+TseganeshYifruBerta@users.noreply.github.com>
2022-01-26 22:54:52 +0300

    Create AdminRegistrationForm.java

---
+++ src/main/java/com/ESA/project/AdminRegistrationForm.java
@@ -0,0 +1,22 @@
+// package com.ESA.project.Admin;
+
+// import org.springframework.security.crypto.password.PasswordEncoder;
+
+// import lombok.Data;
+
+// @Data
+// public class AdminRegistrationForm {
+//     private String username;
+//     private String password;
+//     private String fullName;
+//     private String phone;
+
+//     AdminUsers toUser(PasswordEncoder encoder) {
+//         AdminUsers user = new AdminUsers();
+//         user.setUsername(this.username);
+//         user.setPassword(encoder.encode(this.password));
+//         user.setFullName(this.fullName);
+//         user.setPhone(this.phone);
+//         return user;
+//     }
+// }
